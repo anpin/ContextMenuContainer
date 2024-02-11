4c5283c4d1a82c5d63af06581acc115df1efcc64
Pavel Anpin <pavel@anpin.fyi>
2024-02-11 12:53:54 +0100

    trying to pack on gh

--- pack.ps1
+++ pack.ps1
@@ -1,1 +1,8 @@
-msbuild .\src\APES.UI.XF.csproj -t:Build,Pack -p:Configuration=Release -p:PackageOutputPath="publish"
+#!/usr/bin/env pwsh
+
+param(
+  [Parameter(Mandatory=$True)][String] $PackageOutputPath
+)
+$ErrorActionPreference = "Stop"
+
+msbuild .\src\APES.UI.XF.csproj -t:Build,Pack -p:Configuration=Release -p:PackageOutputPath="$PackageOutputPath"
